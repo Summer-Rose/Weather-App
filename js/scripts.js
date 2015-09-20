cf8ba12599d04ac6c530444849b4229adf716ffb
Summer Brochtrup <sbrochtrup@gmail.com>
2015-09-19 19:23:14 -0700

    Add initial files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// var client = new Usergrid.Client({
+//     orgName:"sbrochtrup",
+//     appName:"weather"
+// });
+
+// var xhr = new XMLHttpRequest();
+// var apiCallAndKey = "http://api.openweathermap.org/data/2.5/weather?q=Portland&APPID=e30a8e64fc1097c94593d32c9c568267";
+// xhr.open("GET", apiCallAndKey, false);
